207092d86c542d910f57d52dc0547fd7a407a2c1
Godfrey Angelo Convento <godfrey-a.convento@gcash.com>
2025-07-03 16:14:58 +0800

    chore: remove unused BankAccount methods

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -31,23 +31,7 @@ public class BankAccount {
         return accNumber;
     }
 
-    public void setAccNumber(String accNumber) {
-        this.accNumber = accNumber;
-    }
-
-    public String getAccHolderName() {
-        return accHolderName;
-    }
-
-    public void setAccHolderName(String accHolderName) {
-        this.accHolderName = accHolderName;
-    }
-
     public double getBalance() {
         return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
     }
 }
